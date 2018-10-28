786818e4e111358f4bb120d1d77168a4da0bfe37
poyu <poyuki9@gmail.com>
2018-10-28 13:55:16 +0300

    feat - finish to develop of sumOfOther functions

--- src/sumOfOther.js
+++ src/sumOfOther.js
@@ -1,8 +1,11 @@
-function sumOfOther(array) {
+module.exports = (array) => {
   const res = [];
-  array.forEach((el,i) => {
-    
+  array.forEach((el, index, arr) => {
+    let reduce = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (array[i] !== el) reduce += array[i];
+    }
+    res.push(reduce);
   });
   return res;
-}
-sumOfOther();
+};
