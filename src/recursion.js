4b228932d1402d10b35b2eaf32b2acf713110030
poyu <poyuki9@gmail.com>
2018-10-28 15:36:37 +0300

    feat - work on function: recursion()

--- src/recursion.js
+++ src/recursion.js
@@ -1,3 +1,15 @@
-module.exports = () => {
+const recursion = (tree) => {
+  const res = [];
+  res.push(tree.value);
 
-}
+  if (tree.left) {
+    const left = recursion(tree.left);
+  }
+  if (tree.right) {
+    const right = recursion(tree.right);
+  }
+
+  return res;
+};
+
+module.exports = recursion;
--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const sumOfOther = require('./src/sumOfOther.js');
 const make = require('./src/make.js');
+const recursion = require('./src/recursion.js');
 
 Object.freeze(assert);
 describe('tests for sumOfOther(array)', () => {
@@ -29,5 +30,15 @@ describe('tests for make()', () => {
   }
   it('at make(15)(34, 21, 666)(41)(sum), should retrun 77', () => {
     assert.equal(make(15)(34, 21, 666)(41)(sum), 77);
+  });
+});
+describe('tests for make()', () => {
+  const tree = {
+    value: 100,
+    left: { value: 90, left: { value: 70 }, right: { value: 99 } },
+    right: { value: 120, left: { value: 110 }, right: { value: 130 } },
+  };
+  it('at make(15)(34, 21, 666)(41)(sum), should retrun 77', () => {
+    assert.equal(recursion(tree), [[100], [90, 120], [70, 99, 110, 130]]);
   });
 });
